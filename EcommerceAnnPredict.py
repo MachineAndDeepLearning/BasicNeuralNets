75f0c28380ab55d33dbc3505140c684fc0a32c56
Krzysztof Jordan <krzysztof.jordan0@gmail.com>
2017-03-14 16:41:02 -0400

    added data, basic feedforward, basic backprop and ecommerce

---
+++ EcommerceAnnPredict.py
@@ -0,0 +1,22 @@
+import numpy as np
+from ProccessData import get_data
+from Util import softmax, init_weight_and_biases, classification_rate
+X, Y = get_data()
+
+M = 5
+D = X.shape[1]
+K = len(set(Y))
+
+W1, b1 = init_weight_and_biases(D, M)
+W2, b2 = init_weight_and_biases(M, K)
+
+def forward(X, W1, b1, W2, b2):
+	Z = np.tanh(X.dot(W1) + b1)
+	return softmax(Z.dot(W2) + b2)
+
+P_Y_given_X = forward(X, W1, b1, W2, b2)
+predictions = np.argmax(P_Y_given_X, axis=1)
+
+print("Classification rate: ", classification_rate(Y, predictions))
+
+
